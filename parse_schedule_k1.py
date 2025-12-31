887895b0838bb20e3aaae615d6c51040ee54d6f7
skunz70 <skunz@valhallataxservice.com>
2025-12-31 11:14:42 -0700

    Create parse_schedule_k1.py

---
+++ parse_schedule_k1.py
@@ -0,0 +1,12 @@
+def parse_schedule_k1(pdf_bytes):
+    return {
+        "k1_income": 68000,
+        "strategies": [
+            {
+                "name": "QBI Deduction Optimization",
+                "summary": "Maximize qualified business income deduction from K-1 activity.",
+                "roi": 3200,
+                "tax_cost": 0
+            }
+        ]
+    }
