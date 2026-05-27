7aac2da39f3a7375cd4244737a09cf5ef81d4815
skunz70 <skunz@valhallataxservice.com>
2026-05-27 13:47:13 -0700

    Update PDF generation function to use new report

--- main.py
+++ main.py
@@ -742,8 +742,8 @@ from report_generator import generate_tax_plan_pdf
 @app.post("/generate_pdf")
 def generate_pdf(payload: dict):
     try:
-        pdf_bytes = generate_tax_plan_pdf(
-    data=payload
+       pdf_bytes = generate_valhalla_report_v2_pdf(
+           data=payload
 )
         return Response(content=pdf_bytes, media_type="application/pdf")
     except Exception as e:
